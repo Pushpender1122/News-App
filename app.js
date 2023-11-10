c5a7a276f2a3900ecfd356cb0ae8c7bb5210990a
Pushpender <Pushpenderjangra2005@gmail.com>
2023-11-10 15:03:59 +0530

    Add fatch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const fetch = require('node-fetch');
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 4500
